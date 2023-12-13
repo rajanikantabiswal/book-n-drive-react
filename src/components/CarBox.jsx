95d32fe7edd2b9b41edec021d66ac09b12b85ce0
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2023-12-13 23:16:43 +0530

    update

--- src/components/CarBox.jsx
+++ src/components/CarBox.jsx
@@ -3,6 +3,7 @@ import CarImage from '../assets/Cars/carImage.webp'
 import Engine from '../assets/icons/engine.svg'
 import Hybrid from '../assets/icons/hybridCar.svg'
 import Road from '../assets/icons/road.svg'
+import { FaCheck } from "react-icons/fa6";
 
 
 
@@ -10,8 +11,8 @@ function CarBox() {
   return (
     <div className='p-4 flex flex-col items-center'>
         <img src={CarImage} alt="" className='w-[85%]' />
-        <div className='flex flex-col items-center'>
-            <div className='grid grid-cols-3 gap-8 w-[50%]'>
+        <div className='flex flex-col items-center bg-[#e5e7eb] py-6'>
+            <div className='grid grid-cols-3 gap-8 w-[60%]'>
                 <div className='flex flex-col justify-between items-center'>
                     <img src={Road} alt=""/>
                     <span className='text-sm'>15KMPL</span>
@@ -24,6 +25,18 @@ function CarBox() {
                     <img src={Engine} alt=""/>
                     <span className='text-sm'>Manual</span>
                 </div> 
+            </div>
+            <div className="border-t border-slate-400 mt-3 py-3 w-[85%] grid grid-cols-2 text-sm" >
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                <span className='flex align-middle'><FaCheck className='mt-1 mr-1'/>Auto Drive</span>
+                
+                
             </div>
         </div>   
     </div>
